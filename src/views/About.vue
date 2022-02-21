728c6a6a5f04c9213887f607e90abaa5d6d43bbf
Santiago <santiagodmaldon@gmail.com>
2022-02-20 23:27:24 -0400

    Using Vuelidate

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,9 @@
             <section id="content">
             <h2 class="title-about">About</h2>
 
+                <div class="info" v-if="agroupInfoUser" v-html="agroupInfoUser"></div>
+
+                <p>INFO BY COMPUTED METHOD</p>
             </section>
 
             <Sidebar></Sidebar>
@@ -29,9 +32,28 @@ import Slider from '../components/SliderComponent.vue';
 import Sidebar from '../components/SidebarComponent.vue';
 export default {
     name: 'About',
+    computed:{
+
+        agroupInfoUser(){
+            const info = "Name: "+this.name+
+                        '<br/>'+
+                        'Last Name: '+this.surname+
+                        '<br>'+
+                        'Age: '+this.year+'<hr>'; 
+
+            return info;
+        }
+    },
     components: {
     Slider,
     Sidebar,
+  },
+  data(){
+      return {
+          name: 'Santiago',
+          surname: 'Maldonado',
+          year: '21 Years Old'
+      }
   }
 }
 </script>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,66 +1,170 @@
 <template>
-<div class="form-vue">
-      <section class="">
+  <div class="center form-flex">
+    <div class="form-vue">
+      <h1 class="subheader">Form</h1>
 
-                <h1 class="subheader">Form</h1>
+      <form class="mid-form">
+        <div class="form-group">
+          <label for="nombre">Name</label>
+          <input type="text" name="name" v-model="user.name" />
 
-                <form class="mid-form ">
-                     <div class="form-group">
-                         <label for="nombre">Name</label>
-                         <input type="text" name="name" />
-                     </div>
- 
-                     <div class="form-group">
-                         <label for="apellidos">Last Name</label>
-                         <input type="text" name="surname" />
-                     </div>
- 
-                     <div class="form-group">
-                         <label for="bio">Biography</label>
-                         <textarea name="bio"></textarea>
-                     </div>
- 
-                     <div class="form-group radiobuttons">
-                         <div class="radio-box">
-                             <input type="radio" name="genero" value="man" id="man" /> 
-                             <label for="man">Man</label>
-                         </div>
- 
-                         <div class="radio-box d-flex">
-                             <input type="radio" name="genero" value="man" id="woman" /> 
-                             <label for="woman">Woman</label>
-                         </div>
- 
-                         <div class="radio-box d-flex">
-                             <input type="radio" name="genero" value="man" id="other" /> 
-                             <label for="other">Other</label>
-                         </div>
- 
-                     </div>
- 
- 
-                     <input type="submit" value="Submit" class="btn-submit  btn-form" />
- 
-                </form>
-                
-            </section>
-</div>
-      
+          <div class="alert-red " v-if="v$.user.name.$error">
+              Invalid Name
+          </div>
+        </div>
 
+        <div class="form-group">
+          <label for="apellidos">Last Name</label>
+          <input type="text" name="surname" v-model="user.surname" />
+           <div class="alert-red " v-if="v$.user.surname.$error">
+               Invalid Surname
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="bio">Biography</label>
+          <textarea name="bio" v-model="user.biography"></textarea>
+             <div class="alert-red " v-if="v$.user.biography.$error">
+               Invalid Biography
+          </div>
+        </div>
+
+        <div class="form-group radiobuttons">
+          <div class="radio-box">
+            <input
+              type="radio"
+              name="genero"
+              value="Man"
+              id="man"
+              v-model="user.gender"
+            />
+            <label for="man">Man</label>
+          </div>
+
+          <div class="radio-box d-flex">
+            <input
+              type="radio"
+              name="genero"
+              value="Woman"
+              id="woman"
+              v-model="user.gender"
+            />
+            <label for="woman">Woman</label>
+          </div>
+
+          <div class="radio-box d-flex">
+            <input
+              type="radio"
+              name="genero"
+              value="Other"
+              id="other"
+              v-model="user.gender"
+            />
+            <label for="other">Other</label>
+          </div>
+        </div>
+
+        <input
+          type="button"
+          value="Submit"
+          @click="showInfo()"
+          class="btn-submit btn-form"
+        />
+      </form>
+    </div>
+
+    <div class="info-form" v-if="show">
+      <h2>Form Info</h2>
+      <div class="info-user" v-html="agroupInfoForm"></div>
+    </div>
+  </div>
 </template>
 
 <style>
-  .form-vue{
-    margin: auto;
-    width: 40%;
-    background: #09a330;
-    border-radius: 20px;
-    margin-bottom: 20px;
-    padding: 10px 00px 20px 0px; 
-  }
+.form-vue {
+  width: 40%;
+  margin-bottom: 50px;
+}
+.form-flex {
+  display: flex;
+  justify-content: space-around;
+}
+.info-user {
+  text-align: start;
+}
+.alert-red{
+    display: inline-block;
+    font-weight: bold;
+    text-align: start;
+    background: red;
+    padding: 0px 10px;
+}
 </style>
 <script>
+import useVuelidate from '@vuelidate/core'
+import { required,minLength } from '@vuelidate/validators'
+
 export default {
-    name: 'Form'
+    name: 'Form',
+    components:{
+    },
+    
+    validations(){
+       return{
+            user:{
+                name: {
+                    required,
+                    minLength: minLength(2)
+                },
+                surname: {
+                    required,
+                    minLength: minLength(2)
+                },
+                biography: {
+                    required,
+                    minLength: minLength(5)
+
+                }        
+            } 
+       } 
+
+    },
+    computed:{
+        agroupInfoForm(){
+            const info= "<strong> Name: "+this.user.name+"</strong> <br/>"+
+                        "<strong> Last Name: "+this.user.surname+"</strong> <br/>"+
+                        "<strong> Gender: "+this.user.gender+"</strong> <br/>"+
+                        "<hr> Biography:  "+this.user.biography;
+
+
+            return info;
+                      
+
+        }
+    },
+    methods:{
+        showInfo(){
+
+            if(this.user.name  && this.user.surname && this.user.biography  && this.user.gender ){
+                this.show = true;
+                this.v$.$validate();
+            }
+         
+      
+        }
+
+    },
+    data(){
+        return{
+            v$: useVuelidate(),
+            user:{
+                name: '',
+                surname: '',
+                biography: '',
+                gender: ''
+            },
+            show : false
+        }
+    }
 }
 </script>
